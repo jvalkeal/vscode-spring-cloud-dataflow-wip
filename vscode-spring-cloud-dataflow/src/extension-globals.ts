19b69ed3f36c0a8fcde56fd3ebf911e45ba15c55
Janne Valkealahti <janne.valkealahti@gmail.com>
2019-06-13 17:38:07 +0100

    Use more globals

--- vscode-spring-cloud-dataflow/src/extension-globals.ts
+++ vscode-spring-cloud-dataflow/src/extension-globals.ts
@@ -3,6 +3,14 @@
 
 export const EXTENSION_ID: string = 'vscode-spring-cloud-dataflow';
 export const CONFIG_PREFIX: string = 'scdf';
+export const LANGUAGE_SCDF_STREAM_PREFIX: string = 'scdfs';
+export const LANGUAGE_SCDF_TASK_PREFIX: string = 'scdft';
+export const LANGUAGE_SERVER_JAR: string = 'spring-cloud-dataflow-language-server.jar';
+export const LANGUAGE_SCDF_DESC: string = 'Language Support for Spring Cloud Data Flow';
+export const COMMAND_SCDF_SERVER_REGISTER: string = 'vscode-spring-cloud-dataflow.server.register';
+export const COMMAND_SCDF_SERVER_UNREGISTER: string = 'vscode-spring-cloud-dataflow.server.unregister';
+export const COMMAND_SCDF_EXPLORER_REFRESH: string = 'vscode-spring-cloud-dataflow.explorer.refresh';
+export const COMMAND_SCDF_STREAMS_SHOW: string = 'vscode-spring-cloud-dataflow.streams.show';
 
 // native os dependant keytar id's
 export namespace keytarConstants {
--- vscode-spring-cloud-dataflow/src/extension.ts
+++ vscode-spring-cloud-dataflow/src/extension.ts
@@ -6,15 +6,24 @@ import { Keytar } from './utils/keytar';
 import { connectServer, disconnectServer } from './server-registrations';
 import { AppsExplorerProvider } from './explorer/apps-explorer-provider';
 import { StreamsExplorerProvider } from './explorer/streams-explorer-provider';
+import {
+    LANGUAGE_SCDF_STREAM_PREFIX, LANGUAGE_SCDF_TASK_PREFIX, CONFIG_PREFIX, LANGUAGE_SERVER_JAR,
+    LANGUAGE_SCDF_DESC, COMMAND_SCDF_SERVER_REGISTER, COMMAND_SCDF_SERVER_UNREGISTER,
+    COMMAND_SCDF_EXPLORER_REFRESH, COMMAND_SCDF_STREAMS_SHOW
+} from './extension-globals';
 
 let languageClient: LanguageClient;
-const jar = 'spring-cloud-dataflow-language-server.jar';
 
 export function activate(context: ExtensionContext) {
-
+    // stash needed global variables to get used in this extension in its lifecycle.
+    // global state is not nice but makes life easier in an extention.
     initializeExtensionGlobals(context);
 
-    const jarPath = Path.resolve(Path.resolve(context.extensionPath), 'out', jar);
+    // we expect server to be plased in 'out' directory, which is a case when real extension
+    // is built and when extension if run within vscode assuming `npm run build` is run which
+    // copies existing scdf language server either from local maven or remote maven repo.
+    const jarPath = Path.resolve(Path.resolve(context.extensionPath), 'out', LANGUAGE_SERVER_JAR);
+
     // let disposable = commands.registerCommand('scdf.test', () => {
     // 	const params = {
     // 		host: 'localhost'
@@ -28,15 +37,16 @@ export function activate(context: ExtensionContext) {
     // });
     // context.subscriptions.push(disposable);
 
-    context.subscriptions.push(commands.registerCommand('vscode-spring-cloud-dataflow.server.register', () => connectServer()));
-    context.subscriptions.push(commands.registerCommand('vscode-spring-cloud-dataflow.server.unregister', disconnectServer));
+    // register needed commands expected to get handled
+    context.subscriptions.push(commands.registerCommand(COMMAND_SCDF_SERVER_REGISTER, () => connectServer()));
+    context.subscriptions.push(commands.registerCommand(COMMAND_SCDF_SERVER_UNREGISTER, disconnectServer));
 
     const appsExplorerProvider = new AppsExplorerProvider();
     window.createTreeView('scdfApps', { treeDataProvider: appsExplorerProvider });
 
     const streamsExplorerProvider = new StreamsExplorerProvider();
     window.createTreeView('scdfStreams', { treeDataProvider: streamsExplorerProvider });
-    context.subscriptions.push(commands.registerCommand('vscode-spring-cloud-dataflow.explorer.refresh', () => {
+    context.subscriptions.push(commands.registerCommand(COMMAND_SCDF_EXPLORER_REFRESH, () => {
         appsExplorerProvider.refresh();
         streamsExplorerProvider.refresh();
     }));
@@ -46,7 +56,7 @@ export function activate(context: ExtensionContext) {
 
     // for scdfs language
     context.subscriptions.push(workspace.registerTextDocumentContentProvider('scdfs', streamsExplorerProvider));
-    commands.registerCommand('vscode-spring-cloud-dataflow.streams.show', resource => {
+    commands.registerCommand(COMMAND_SCDF_STREAMS_SHOW, resource => {
         window.showTextDocument(resource.getResourceUri());
     });
 
@@ -61,10 +71,13 @@ export function activate(context: ExtensionContext) {
         }
     };
     const clientOptions: LanguageClientOptions = {
-        documentSelector: ["scdfs", "scdft"]
+        documentSelector: [
+            LANGUAGE_SCDF_STREAM_PREFIX,
+            LANGUAGE_SCDF_TASK_PREFIX
+        ]
     };
 
-    languageClient = new LanguageClient('scdf', 'Language Support for Spring Cloud Data Flow', serverOptions, clientOptions);
+    languageClient = new LanguageClient(CONFIG_PREFIX, LANGUAGE_SCDF_DESC, serverOptions, clientOptions);
     languageClient.start();
 }
 
