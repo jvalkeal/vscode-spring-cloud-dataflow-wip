dedd455dbf7ee4a6da254804df890d2846284b09
Janne Valkealahti <janne.valkealahti@gmail.com>
2019-08-29 16:15:53 +0300

    Remove di from core

--- vscode-extension-core/src/ui/status-bar-manager.ts
+++ vscode-extension-core/src/ui/status-bar-manager.ts
@@ -13,10 +13,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import { injectable } from "inversify";
 import { StatusBarItem, StatusBarAlignment, window } from "vscode";
 
-@injectable()
 export class StatusBarManager {
 
     private statusBarItem: StatusBarItem;
